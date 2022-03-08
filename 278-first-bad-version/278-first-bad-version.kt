d7e2332f600d9803dbb6bc7d66fe34ffcd303ed4
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2022-03-08 23:52:08 +0200

    Time: 362 ms (27.63%), Space: 33.7 MB (25.61%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.kt
+++ 278-first-bad-version/278-first-bad-version.kt
@@ -5,7 +5,7 @@ class Solution: VersionControl() {
     override fun firstBadVersion(n: Int) : Int {
         var right = n
         var left = 1
-        while (right > left) {
+        while (right != left) {
             val median = left + (right - left) / 2
             val medianIsBad = isBadVersion(median)
             if (medianIsBad) {
