78e82d449a799acbf6fe39f49e58882150427c7c
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2022-03-09 21:43:42 +0200

    Time: 391 ms, Memory: 47.1 MB - LeetHub

--- add-two-numbers/add-two-numbers.kt
+++ add-two-numbers/add-two-numbers.kt
@@ -9,35 +9,6 @@
  */
 class Solution {
     fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
-//         var result: ListNode? = null
-//         var node1 = l1
-//         var node2 = l2
-//         var carry = 0
-//         var lastNode: ListNode? = null
-//         while (node1 != null || node2 != null) {
-//             val value1 = node1?.`val` ?: 0
-//             val value2 = node2?.`val` ?: 0
-//             var sum = value1 + value2 + carry
-//             if (sum >= 10) {
-//                 carry = 1
-//                 sum = sum % 10
-//             } else {
-//                 carry = 0
-//             }
-//             if (result == null) {
-//                 result = ListNode(sum)
-//                 lastNode = result
-//             } else {
-//                 lastNode?.next = ListNode(sum)
-//                 lastNode = lastNode?.next
-//             }
-            
-//             node1 = node1?.next
-//             node2 = node2?.next
-//         }
-//         if (carry > 0) {
-//             lastNode?.`val` = carry
-//         }
         return getSum(l1, l2, 0)
     }
     
