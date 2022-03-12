eaaed50348603bd68de01724432a4bdec028ed50
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2022-03-12 15:05:47 +0200

    Time: 319 ms, Memory: 33.6 MB - LeetHub

---
+++ first-bad-version/first-bad-version.kt
@@ -0,0 +1,19 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      fun isBadVersion(version: Int) : Boolean {} */
+
+class Solution: VersionControl() {
+    override fun firstBadVersion(n: Int) : Int {
+        var right = n
+        var left = 1
+        while (right != left) {
+            val median = left + (right - left) / 2
+            val medianIsBad = isBadVersion(median)
+            if (medianIsBad) {
+                right = median
+            } else {
+                left = median + 1
+            }
+        }
+        return left
+	}
+}
