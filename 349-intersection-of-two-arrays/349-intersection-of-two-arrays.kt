655c7362bee552f2a684e78f2f7a0e695ae2e0ab
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2022-03-16 23:16:54 +0200

    Time: 348 ms (45.33%), Space: 38.7 MB (66.19%) - LeetHub

--- 349-intersection-of-two-arrays/349-intersection-of-two-arrays.kt
+++ 349-intersection-of-two-arrays/349-intersection-of-two-arrays.kt
@@ -1,18 +1,23 @@
 class Solution {
     fun intersection(nums1: IntArray, nums2: IntArray): IntArray {
-        Arrays.sort(nums2);
-        val set = hashSetOf<Int>();
-        val setNotExists = hashSetOf<Int>();
-        nums1.forEach {
-            if (setNotExists.contains(it) || set.contains(it)) return@forEach
-            if (nums2.binarySearch(it) < 0) {
-                setNotExists.add(it)
+        Arrays.sort(nums1); // O(nlog(N))
+        Arrays.sort(nums2); // O(nlog(N))
+        var x = 0
+        var y = 0
+        val result = arrayListOf<Int>()
+        while (x < nums1.size && y < nums2.size) {
+            if (nums1[x] > nums2[y]) {
+                y++
+            } else if (nums1[x] < nums2[y]) {
+                x++
             } else {
-                set.add(it)
+                if (result.size == 0 || result.last() != nums1[x]) {
+                    result.add(nums1[x])
+                }
+                x++
+                y++
             }
         }
-        val result = set.toIntArray()
-        result.sort()
-        return result
+        return result.toIntArray()
     }
 }
