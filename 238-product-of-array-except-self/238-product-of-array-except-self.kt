8fc28f51830aa9d72d925e40d632b2090477ab48
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2022-03-16 19:05:49 +0200

    Time: 540 ms (77.06%), Space: 70.3 MB (63.92%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.kt
+++ 238-product-of-array-except-self/238-product-of-array-except-self.kt
@@ -1,18 +1,15 @@
 class Solution {
     fun productExceptSelf(nums: IntArray): IntArray {
         val left = Array<Int>(nums.size) { 0 }
-        var leftProduct = 1
-
-        for (i in 0 until nums.size) {
-            left[i] = leftProduct
-            leftProduct = leftProduct * nums[i]
+        left[0] = 1
+        for (i in 1 until nums.size) {
+            left[i] = left[i-1] * nums[i-1]
         }
         
         val right = Array<Int>(nums.size) { 0 }
-        var rightProduct = 1
-        for (i in nums.size-1 downTo 0) {
-            right[i] = rightProduct
-            rightProduct = rightProduct * nums[i]
+        right[nums.size - 1] = 1
+        for (i in nums.size-2 downTo 0) {
+            right[i] = nums[i+1] * right[i+1]
         }
         
         val result = Array<Int>(nums.size) { 0 }
