9702893864673c7e4f5304594b6d44fc03a91b53
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2022-05-26 12:00:18 +0400

    Time: 253 ms (32.43%), Space: 34.7 MB (96.76%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.kt
+++ 58-length-of-last-word/58-length-of-last-word.kt
@@ -1,5 +1,14 @@
 class Solution {
     fun lengthOfLastWord(s: String): Int {
-        return s.split(" ").filter { it.length > 0 }.lastOrNull()?.length ?: 0
+        var result = 0
+        for (i in s.length - 1 downTo 0) {
+            if (result > 0 && s[i] == ' ') {
+                break
+            }
+            if (s[i] != ' ') {
+                result++
+            }
+        }
+        return result
     }
 }
