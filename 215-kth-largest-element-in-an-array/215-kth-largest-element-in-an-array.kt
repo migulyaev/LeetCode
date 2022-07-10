2c8e6baa80fc651dc37caeb661f62f28f3b0f345
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2022-07-10 15:11:52 +0400

    Time: 378 ms (45.65%), Space: 41.7 MB (48.93%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.kt
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.kt
@@ -1,6 +1,13 @@
 class Solution {
     fun findKthLargest(nums: IntArray, k: Int): Int {
-        nums.sort()
-        return nums[nums.size-k]
+        val heap = PriorityQueue<Int> { o1, o2 -> o1!! - o2!! }
+        
+        for (i in nums) {
+            heap.add(i)
+            if (heap.size > k) {
+                heap.poll()
+            }
+        }
+        return heap.poll()
     }
 }
