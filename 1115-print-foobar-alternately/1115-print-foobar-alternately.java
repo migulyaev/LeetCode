882d834a095a63d5a2d95f407e8294538c730378
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2022-07-24 14:14:23 +0300

    Time: 46 ms (33.19%), Space: 44.7 MB (22.38%) - LeetHub

--- 1115-print-foobar-alternately/1115-print-foobar-alternately.java
+++ 1115-print-foobar-alternately/1115-print-foobar-alternately.java
@@ -1,6 +1,9 @@
 class FooBar {
     private int n;
-    private AtomicInteger counter = new AtomicInteger(0);
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition printFooCondition = lock.newCondition();
+    private Condition printBarCondition = lock.newCondition();
+    private boolean isBarPrinted = true;
 
     public FooBar(int n) {
         this.n = n;
@@ -9,24 +12,36 @@ class FooBar {
     public void foo(Runnable printFoo) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-            while (counter.get() != 0) {
-                // wait
+            try {
+                lock.lock();
+                while (!isBarPrinted) {
+                    printFooCondition.await();
+                }
+                // printFoo.run() outputs "foo". Do not change or remove this line.
+                printFoo.run();
+                isBarPrinted = false;
+                printBarCondition.signalAll();
+            } finally {
+                lock.unlock();
             }
-        	// printFoo.run() outputs "foo". Do not change or remove this line.
-        	printFoo.run();
-            counter.incrementAndGet();
         }
     }
 
     public void bar(Runnable printBar) throws InterruptedException {
         
         for (int i = 0; i < n; i++) {
-            while (counter.get() != 1) {
-                // wait
+            try {
+                lock.lock();
+                while (isBarPrinted) {
+                    printBarCondition.await();
+                }
+                // printBar.run() outputs "bar". Do not change or remove this line.
+                printBar.run();
+                isBarPrinted = true;
+                printFooCondition.signalAll();
+            } finally {
+                lock.unlock();
             }
-            // printBar.run() outputs "bar". Do not change or remove this line.
-        	printBar.run();
-            counter.decrementAndGet();
         }
     }
 }
