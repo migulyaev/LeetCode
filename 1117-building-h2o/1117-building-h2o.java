a011ab1ea20750f576308aae4dfa6c318d20c35c
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2022-07-24 15:33:06 +0300

    Time: 34 ms (46.07%), Space: 45.3 MB (69.50%) - LeetHub

--- 1117-building-h2o/1117-building-h2o.java
+++ 1117-building-h2o/1117-building-h2o.java
@@ -1,70 +1,23 @@
 class H2O {
     
-    private ReentrantLock lock = new ReentrantLock();
-    private Condition printHydro = lock.newCondition();
-    private Condition printOxygen = lock.newCondition();
-    private int hCounter = 0;
-    private int oCounter = 1;
+    private Semaphore hydrogen, oxygen;
 
     public H2O() {
-
+        hydrogen = new Semaphore(2);
+        oxygen = new Semaphore(0);
     }
 
     public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-        try {
-            lock.lock();
-            while (oCounter == 0) {
-                printHydro.await();
-            }
-            // releaseHydrogen.run() outputs "H". Do not change or remove this line.
-            releaseHydrogen.run();
-            hCounter++;
-            if (hCounter == 2) {
-                oCounter = 0;
-                printOxygen.signalAll();
-            }
-        } finally {
-            lock.unlock();
-        }
+		hydrogen.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        oxygen.release();
     }
 
     public void oxygen(Runnable releaseOxygen) throws InterruptedException {
-        try {
-            lock.lock();
-            while (hCounter != 2) {
-                printOxygen.await();
-            }
-            // releaseOxygen.run() outputs "O". Do not change or remove this line.
-            releaseOxygen.run();
-            oCounter = 1;
-            hCounter = 0;
-            printHydro.signalAll();
-        } finally {
-            lock.unlock();
-        }
+        oxygen.acquire(2);
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+		releaseOxygen.run();
+        hydrogen.release(2);
     }
 }
-
-// class H2O {
-    
-//     private Semaphore hydrogen, oxygen;
-
-//     public H2O() {
-//         hydrogen = new Semaphore(2);
-//         oxygen = new Semaphore(0);
-//     }
-
-//     public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
-// 		hydrogen.acquire();
-//         // releaseHydrogen.run() outputs "H". Do not change or remove this line.
-//         releaseHydrogen.run();
-//         oxygen.release();
-//     }
-
-//     public void oxygen(Runnable releaseOxygen) throws InterruptedException {
-//         oxygen.acquire(2);
-//         // releaseOxygen.run() outputs "O". Do not change or remove this line.
-// 		releaseOxygen.run();
-//         hydrogen.release(2);
-//     }
-// }
