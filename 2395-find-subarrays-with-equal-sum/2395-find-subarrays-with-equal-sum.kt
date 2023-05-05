79397face9b74ba811c6655d929626cb77e24233
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2023-05-05 13:27:15 +0200

    Time: 150 ms (50.00%), Space: 35.7 MB (25.00%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.kt
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.kt
@@ -1,10 +1,12 @@
 class Solution {
     fun findSubarrays(nums: IntArray): Boolean {
-        for (i in 0 until nums.size - 2) {
-            for (k in i+1 until nums.size - 1) {
-                if (nums[i] + nums[i+1] == nums[k] + nums[k+1]) {
-                    return true
-                }
+        val results = hashSetOf<Int>()
+        for (i in 0 until nums.size - 1) {
+            val sum = nums[i] + nums[i+1]
+            if (results.contains(sum)) {
+                return true
+            } else {
+                results.add(sum)
             }
         }
         return false
