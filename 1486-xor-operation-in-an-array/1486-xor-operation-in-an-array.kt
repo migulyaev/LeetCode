a326728809515640462d89ce6f0369970470055e
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2023-07-23 13:26:19 +0200

    Time: 120 ms (100.00%), Space: 33.2 MB (33.33%) - LeetHub

---
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.kt
@@ -0,0 +1,9 @@
+class Solution {
+    fun xorOperation(n: Int, start: Int): Int {
+        var result = 0
+        for (i in 0 until n) {
+            result = result.xor(start + 2 * i)
+        }
+        return result
+    }
+}
