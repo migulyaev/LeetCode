1090ac3853e7711fde43b241ae8c38ebb03c26c6
Mikhail Gulyaev <48011967+mgulyaev10@users.noreply.github.com>
2023-07-24 12:18:55 +0200

    Time: 290 ms (96.77%), Space: 38 MB (83.87%) - LeetHub

---
+++ 2697-lexicographically-smallest-palindrome/2697-lexicographically-smallest-palindrome.kt
@@ -0,0 +1,14 @@
+class Solution {
+    fun makeSmallestPalindrome(s: String): String {
+        val median = s.length / 2
+        val stringBuilder = StringBuilder(s)
+        for (i in 0..median) {
+            if (s[i] < s[s.length-i-1]) {
+                stringBuilder[s.length-i-1] = s[i]
+            } else {
+                stringBuilder[i] = s[s.length-i-1]
+            }
+        }
+        return stringBuilder.toString()
+    }
+}
