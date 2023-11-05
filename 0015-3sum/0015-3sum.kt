020466f8e227310d7cd459f488e0a227a8294b85
Mikhail Guliaev <48011967+mgulyaev10@users.noreply.github.com>
2023-11-05 14:41:40 +0100

    Time: 1813 ms (12.49%), Space: 61.8 MB (14.20%) - LeetHub

---
+++ 0015-3sum/0015-3sum.kt
@@ -0,0 +1,28 @@
+class Solution {
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        nums.sort()
+        val result = hashSetOf<List<Int>>()
+        for (i in 0 until nums.size - 2) {
+            val twoSum = twoSum(nums.copyOfRange(i + 1, nums.size), -nums[i])
+            twoSum.forEach {
+                if (it.size == 2) {
+                    result.add(listOf(it[0], it[1], nums[i]).sorted())
+                }
+            }
+        }
+        return result.toList()
+    }
+    
+    private fun twoSum(nums: IntArray, target: Int): List<List<Int>> {
+        val result = arrayListOf<List<Int>>()
+        val numbers = hashMapOf<Int, Int>()
+        nums.forEachIndexed { index, num ->
+            val needToTarget = target - num
+            if (numbers.containsKey(needToTarget)) {
+                result.add(listOf(num, needToTarget!!))
+            }
+            numbers[num] = index
+        }
+        return result
+    }
+}
