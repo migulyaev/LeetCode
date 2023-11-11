515706f7b356eec2d3ac6ad7aa7cdc8691b10bd4
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2023-11-11 22:41:24 +0100

    Time: 265 ms (87.25%), Space: 41 MB (73.94%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.kt
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.kt
@@ -1,35 +1,21 @@
 class Solution {
     fun sortedSquares(nums: IntArray): IntArray {
         val result = IntArray(nums.size) { 0 }
-        var positive = -1
-        for (i in 0 until nums.size) {
-            if (nums[i] >= 0) {
-                positive = i
-                break
+
+        var right = nums.size - 1
+        var left = 0
+
+        for (i in nums.size - 1 downTo(0)) {
+            var square: Int = 0
+            if (abs(nums[left]) < abs(nums[right])) {
+                result[i] = nums[right] * nums[right]
+                right--
+            } else {
+                result[i] = nums[left] * nums[left]
+                left++
             }
         }
         
-        var negative = if (positive != -1) positive - 1 else nums.size - 1
-        
-        var counter = 0
-        while (positive < nums.size && positive != -1 || negative != -1) {
-            if (positive < nums.size && positive != -1 && negative >= 0) {
-                if (nums[positive] < abs(nums[negative])) {
-                    result[counter] = nums[positive] * nums[positive]
-                    positive++
-                } else {
-                    result[counter] = nums[negative] * nums[negative]
-                    negative--
-                }
-            } else if (positive == nums.size || positive == -1) {
-                    result[counter] = nums[negative] * nums[negative]
-                    negative--
-            } else if (negative < 0) {
-                    result[counter] = nums[positive] * nums[positive]
-                    positive++
-            }
-            counter++
-        }
         return result
     }
 }
