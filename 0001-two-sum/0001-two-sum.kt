4fadb30126e8ac1222651fc9c8178a09c4fae1e1
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-08-21 22:07:52 +0100

    Time: 188 ms (90.73%), Space: 37.7 MB (93.39%) - LeetHub

--- 0001-two-sum/0001-two-sum.kt
+++ 0001-two-sum/0001-two-sum.kt
@@ -1,11 +1,12 @@
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
-        for (i in 0 until nums.size - 1) {
-            for (k in i + 1 until nums.size) {
-                if (nums[i] + nums[k] == target) {
-                    return intArrayOf(i, k)
-                }
+        val indices = hashMapOf<Int, Int>()
+        for (i in 0 until nums.size) {
+            val needToFind = target - nums[i]
+            if (indices.containsKey(needToFind)) {
+                return intArrayOf(i, indices[needToFind]!!)
             }
+            indices[nums[i]] = i
         }
         return intArrayOf()
     }
