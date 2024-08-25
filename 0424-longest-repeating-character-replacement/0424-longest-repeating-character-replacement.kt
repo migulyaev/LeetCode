2fabda735baf8ca3573a9fbf4920f01554ed7b1c
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-08-25 17:19:05 +0100

    Time: 182 ms (77.34%), Space: 36.9 MB (76.26%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.kt
@@ -0,0 +1,23 @@
+class Solution {
+    fun characterReplacement(s: String, k: Int): Int {
+        if (s.length == 1) {
+            return 1
+        }
+        val chars = Array<Int>(26) { 0 }
+        var left = 0
+        var right = 0
+        var maxCountInWindow = 0
+        while (right < s.length) {
+            val next = s[right]
+            chars[next - 'A'] = chars[next - 'A'] + 1
+            maxCountInWindow = max(maxCountInWindow, chars[next - 'A'])
+            val length = right - left + 1
+            if (length - maxCountInWindow > k) {
+                chars[s[left] - 'A'] = chars[s[left] - 'A'] - 1
+                left++
+            }
+            right++
+        }
+        return right - left
+    }
+}
