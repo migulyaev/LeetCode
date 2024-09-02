a9847c28b659a791288a6371262eab11e3d60cc7
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-09-02 11:53:23 +0100

    Time: 437 ms (28.64%), Space: 33.6 MB (91.75%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.kt
+++ 0647-palindromic-substrings/0647-palindromic-substrings.kt
@@ -7,12 +7,11 @@ class Solution {
                 var start = i
                 var end = k
                 while (start < end) {
-                    if (s[start] == s[end]) {
-                        start++
-                        end--
-                    } else {
+                    if (s[start] != s[end]) {
                         break
                     }
+                    start++
+                    end--
                 }
                 if (start >= end) {
                     result++
