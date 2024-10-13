28c9d43e9c343d9c44dbeb53a2ee5be604cf1f65
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-10-13 13:22:57 +0100

    Time: 172 ms (87.01%), Space: 38.5 MB (85.83%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.kt
+++ 0338-counting-bits/0338-counting-bits.kt
@@ -1,17 +1,9 @@
 class Solution {
     fun countBits(n: Int): IntArray {
         val result = IntArray(n + 1) { 0 }
-        for (i in 0..n) {
-            var bits = 0
-            var temp = i
-            while (temp > 0) {
-                val bit = temp % 2
-                if (bit > 0) {
-                    bits++
-                }
-                temp /= 2
-            }
-            result[i] = bits
+        result[0] = 0
+        for (i in 1..n) { // O(N)
+            result[i] = result[i.and(i-1)] + 1
         }
         return result
     }
