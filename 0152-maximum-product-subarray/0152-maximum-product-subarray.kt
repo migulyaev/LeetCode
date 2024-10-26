33fa00789253d3f3d58ba9df571031751837d8b0
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-10-26 12:21:46 +0100

    Time: 3 ms (97.42%), Space: 37.8 MB (81.12%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.kt
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.kt
@@ -1,13 +1,20 @@
 class Solution {
     fun maxProduct(nums: IntArray): Int {
-        var max = nums[0]
+        var result = nums[0]
+        var curMax = 1
+        var curMin = 1
         for (i in 0 until nums.size) {
-            var product = 1
-            for (k in i until nums.size) {
-                product = product * nums[k]
-                max = max(max, product)
+            if (nums[i] == 0) {
+                curMax = 1
+                curMin = 1
+                result = max(0, result)
+                continue
             }
+            val tmpCurMax = curMax
+            curMax = max(max(curMin * nums[i], curMax * nums[i]), nums[i])
+            curMin = min(min(tmpCurMax * nums[i], curMin * nums[i]), nums[i])
+            result = max(curMax, result)
         }
-        return max
+        return result
     }
 }
