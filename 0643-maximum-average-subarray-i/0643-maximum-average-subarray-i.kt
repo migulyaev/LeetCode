14cae921c81005328984975684f634009eec80ba
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-11-30 21:29:53 +0300

    Time: 5 ms (72.06%), Space: 52.2 MB (49.64%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.kt
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.kt
@@ -1,14 +1,17 @@
 class Solution {
     fun findMaxAverage(nums: IntArray, k: Int): Double {
-        var result = -100001.0
-        for (i in 0 until nums.size - k + 1) {
-            var sum = 0
-            for (f in i until i + k) {
-                sum += nums[f]
-            }
-            val avg = sum.toDouble() / k
-            result = max(avg, result)
+        var currentSum = 0.0
+        for (i in 0 until k) {
+            currentSum += nums[i]
         }
-        return result
+        var max = currentSum / k
+        var endPointer = k
+        
+        while (endPointer < nums.size) {
+            currentSum = currentSum + (-nums[endPointer - k] + nums[endPointer])
+            max = max(currentSum / k, max)
+            endPointer++
+        }
+        return max
     }
 }
