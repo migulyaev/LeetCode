f8c60771069424250a92f89762b5e7a96587b37d
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-12-02 01:03:23 +0300

    Time: 0 ms (100.00%), Space: 38.9 MB (5.95%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.kt
+++ 0206-reverse-linked-list/0206-reverse-linked-list.kt
@@ -9,22 +9,21 @@
  */
 class Solution {
     fun reverseList(head: ListNode?): ListNode? {
-        head ?: return null
-        var prev = head
-        var next = head.next
-        prev.next = null
-        while (next != null) {
-            val temp = next.next
-            next.next = prev
-            prev = next
-            next = temp
+        if (head == null) {
+            return null
         }
+        if (head.next == null) {
+            return head
+        }
+        var prev = head
+        var node = head.next
+        head.next = null
+        do {
+            val tempNext = node?.next
+            node?.next = prev
+            prev = node
+            node = tempNext
+        } while (node != null)
         return prev
     }
 }
-
-// prev = 1, next = 2
-// temp = 3; 2.next = 1; prev = 2; next = 3
-// temp = 4; 3.next = 2; prev = 3; next = 4
-// temp = 5; 4.next = 3; prev = 4; next = 5
-// temp = null; 5.next = 4; prev = 5; next = null
