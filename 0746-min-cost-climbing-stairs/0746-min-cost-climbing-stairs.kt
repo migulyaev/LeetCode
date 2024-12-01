06363882bb24fc5b0f8517a587078b50b9a12768
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-12-01 21:18:01 +0300

    Time: 17 ms (26.71%), Space: 38.6 MB (8.51%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.kt
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.kt
@@ -3,21 +3,10 @@ class Solution {
         val minCosts = IntArray(cost.size) { -1 }
         minCosts[0] = 0
         minCosts[1] = 0
-        calculate(cost, minCosts, cost.size - 1)
+        for (i in 2 until cost.size) {
+            minCosts[i] = min(minCosts[i - 2] + cost[i - 2], minCosts[i - 1] + cost[i - 1])
+        }
         return min(cost.last() + minCosts.last(), cost[cost.size - 2] + minCosts[minCosts.size - 2])
     }
     
-    private fun calculate(cost: IntArray, mins: IntArray, index: Int) {
-        if (index == 0 || index == 1) {
-            return
-        }
-        if (mins[index] != -1) {
-            return
-        }
-        calculate(cost, mins, index - 1)
-        calculate(cost, mins, index - 2)
-        val fromTwoStepsDown = mins[index - 2] + cost[index - 2]
-        val fromOneStepDown = mins[index - 1] + cost[index - 1]
-        mins[index] = min(fromTwoStepsDown, fromOneStepDown)
-    }
 }
