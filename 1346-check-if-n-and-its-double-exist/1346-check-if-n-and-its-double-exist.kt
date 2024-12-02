e05b5d40ad12ac0ef60bd534c9db2e698345c7cd
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-12-03 02:03:09 +0300

    Time: 3 ms (78.87%), Space: 37.3 MB (85.47%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.kt
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.kt
@@ -2,13 +2,9 @@ class Solution {
     fun checkIfExist(arr: IntArray): Boolean {
         val set = hashSetOf<Int>()
         for (i in 0 until arr.size) {
-            if (set.contains(0) && arr[i] == 0) return true
-            set.add(arr[i])
-        }
-        for (i in 0 until arr.size) {
-            if (arr[i] == 0) continue
             if (set.contains(arr[i] * 2)) return true
             if (arr[i] % 2 == 0 && set.contains(arr[i] / 2)) return true
+            set.add(arr[i])
         }
         return false
     }
