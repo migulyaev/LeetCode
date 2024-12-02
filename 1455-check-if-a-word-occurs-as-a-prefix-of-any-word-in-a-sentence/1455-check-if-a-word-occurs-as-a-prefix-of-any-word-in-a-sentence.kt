efaf8ba72af8bee460654ef35eebf195c4d5e84c
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-12-03 01:52:34 +0300

    Time: 0 ms (100.00%), Space: 34.4 MB (93.33%) - LeetHub

--- 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.kt
+++ 1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence/1455-check-if-a-word-occurs-as-a-prefix-of-any-word-in-a-sentence.kt
@@ -1,5 +1,27 @@
 class Solution {
     fun isPrefixOfWord(sentence: String, searchWord: String): Int {
-        return sentence.split(" ").indexOfFirst { it.startsWith(searchWord) }.takeIf { it >= 0 }?.plus(1) ?: -1
+        var i = 0
+        var wordIndex = 0
+        while (i < sentence.length) {
+            while (i < sentence.length && sentence[i] == ' ') i++
+            while (i < sentence.length && sentence[i] != ' ') {
+                var p = 0
+                wordIndex++
+                while (i < sentence.length && p < searchWord.length && sentence[i] != ' ') {
+                    if (sentence[i] == searchWord[p]) {
+                        i++
+                        p++
+                    } else {
+                        while (i < sentence.length && sentence[i] != ' ') i++
+                        break
+                    }
+                }
+                if (p == searchWord.length) {
+                    return wordIndex
+                }
+                i++
+            }
+        }
+        return -1
     }
 }
