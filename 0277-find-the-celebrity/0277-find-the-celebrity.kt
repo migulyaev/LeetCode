c11398428de5bce3357579e88d291481d2c8e738
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-12-08 02:29:01 +0300

    Time: 274 ms (45.76%), Space: 48.1 MB (28.36%) - LeetHub

---
+++ 0277-find-the-celebrity/0277-find-the-celebrity.kt
@@ -0,0 +1,27 @@
+/* The knows API is defined in the parent class Relation.
+      fun knows(a: Int, b: Int) : Boolean {} */
+
+class Solution: Relation() {
+    override fun findCelebrity(n: Int) : Int {
+        for (i in 0 until n) {
+            var knownByEveryone = true
+            var knowsNoone = true
+            for (k in 0 until n) {
+                if (i == k) continue
+                if (!knows(k, i)) {
+                    knownByEveryone = false
+                    break
+                }
+            }
+            for (k in 0 until n) {
+                if (i == k) continue
+                if (knows(i, k)) {
+                    knowsNoone = false
+                    break
+                }
+            }
+            if (knownByEveryone && knowsNoone) return i
+        }
+        return -1
+	}
+}
