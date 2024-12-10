c6c6081fac2942f990b9bc48744ea91f290be691
Mikhail Guliaev <48011967+migulyaev@users.noreply.github.com>
2024-12-10 23:53:55 +0300

    Time: 42 ms (33.33%), Space: 41.7 MB (25.00%) - LeetHub

--- 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.kt
+++ 2981-find-longest-special-substring-that-occurs-thrice-i/2981-find-longest-special-substring-that-occurs-thrice-i.kt
@@ -1,45 +1,53 @@
 class Solution {
     fun maximumLength(s: String): Int {
-    val sequencesQueue = ArrayDeque<Pair<Char, Int>>()
-    var count = 1
+        val sequencesQueue = ArrayDeque<Pair<Char, Int>>()
+        var count = 1
 
-    // Build sequences of consecutive characters
-    for (i in 1..s.length) {
-        if (i == s.length || s[i] != s[i - 1]) {
-            sequencesQueue.addFirst(Pair(s[i - 1], count))
-            count = 1
-        } else {
-            count++
+        for (i in 1..s.length) {
+            if (i == s.length || s[i] != s[i - 1]) {
+                sequencesQueue.addFirst(Pair(s[i - 1], count))
+                count = 1
+            } else {
+                count++
+            }
         }
-    }
 
-    // Group subsequences by character and their lengths
-    val goodSubstrings = sequencesQueue.groupBy({ it.first }, { it.second })
-        .mapValues { ArrayList(it.value) }
-        
+        val goodSubstrings = sequencesQueue.groupBy({ it.first }, { it.second })
+            .mapValues { ArrayList(it.value) }
+
         var max = -1
-        for (key in goodSubstrings.keys) {
-            val substrings = goodSubstrings[key]!!
+        for ((_, substrings) in goodSubstrings) {
             substrings.sort()
+            when (substrings.size) {
+                1 -> {
+                    if (substrings.first() < 3) continue
+                    if (substrings.first() == 3) {
+                        max = max(max, 1)
+                        continue
+                    }
+                    max = max(max, substrings.first() - 2)
+                }
+                2 -> {
+                    if (substrings.first() == 1 && substrings.last() == 1) continue
+                    if (substrings.first() == substrings.last()) {
+                        max = max(max, substrings.last() - 1)
+                        continue
+                    }
+                    val potentialString = if (substrings.last() - 1 <= substrings.first()) {
+                        substrings.last() - 1
+                    } else {
+                        -1
+                    }
+                    max = max(max, max(substrings.last() - 2, potentialString))
+                }
+                3 -> {
+                    
+                }
+            }
             if (substrings.size == 1) {
-                if (substrings.first() < 3) continue
-                if (substrings.first() == 3) {
-                    max = max(max, 1)
-                    continue
-                }
-                max = max(max, substrings.first() - 2)
+
             } else if (substrings.size == 2) {
-                if (substrings.first() == 1 && substrings.last() == 1) continue
-                if (substrings.first() == substrings.last()) {
-                    max = max(max, substrings.last() - 1)
-                    continue
-                }
-                val potentialString = if (substrings.last() - 1 <= substrings.first()) {
-                    substrings.last() - 1
-                } else {
-                    -1
-                }
-                max = max(max, max(substrings.last() - 2, potentialString))
+                
             } else {
                 max = max(max, substrings[substrings.size - 3])
                 val potentialString = if (substrings.last() - 1 <= substrings[substrings.size - 2]) {
